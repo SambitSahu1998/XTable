15ff1e8a215882e3647846d412c9ae53ea601b43
Sambit Sahu <Sambit.Sahu@yovant.com>
2024-06-05 12:16:14 +0530

    Date and Views Table Updates

---
+++ xtable/src/Table.jsx
@@ -0,0 +1,62 @@
+import React,{useState} from 'react';
+
+const data = [
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" }
+];
+
+const Table = () => {
+    
+    const [tableData, setTableData] = useState(data);
+
+    const sortByDate = () => {
+        const sortedData = [...tableData].sort((a, b) => {
+            if (a.date === b.date) {
+                return b.views - a.views;
+            }
+            return new Date(b.date) - new Date(a.date);
+        });
+        setTableData(sortedData);
+    };
+
+    const sortByViews = () => {
+        const sortedData = [...tableData].sort((a, b) => {
+            if (a.views === b.views) {
+                return new Date(b.date) - new Date(a.date);
+            }
+            return b.views - a.views;
+        });
+        setTableData(sortedData);
+    };
+
+  return (
+    <div>
+        <h1>Date and Views Table</h1>
+	    <button onClick={sortByDate}>Sort by Date</button>
+	    <button onClick={sortByViews}>Sort by Views</button>
+        <table style={{border:"none"}}>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Views</th>
+                    <th>Article</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableData.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.date}</td>
+                        <td>{item.views}</td>
+                        <td>{item.article}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default Table;
